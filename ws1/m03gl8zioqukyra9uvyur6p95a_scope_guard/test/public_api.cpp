93063cb8b32408d3ad2c679a321f865dd240c292
Érchegyi Dávid <celicaw8@gmail.com>
2026-08-08 21:06:02 +0100

    test scope guard public API

--- ws1/m03gl8zioqukyra9uvyur6p95a_scope_guard/test/public_api.cpp
+++ ws1/m03gl8zioqukyra9uvyur6p95a_scope_guard/test/public_api.cpp
@@ -1,10 +1,29 @@
 #include <m03gn97n4iusbtl7uthb01wu9m_test_framework/test_framework.h>
 #include <m03gl8zioqukyra9uvyur6p95a_scope_guard/api.h>
 
+#include <functional>
+
+namespace api = m03gl8zioqukyra9uvyur6p95a_scope_guard;
 namespace test = m03gn97n4iusbtl7uthb01wu9m_test_framework;
 
 int main() {
     return test::run([] {
-        test::expect(true, "public API headers compile");
+        int cleanup_count = 0;
+        {
+            api::scope_guard_t guard([&] { ++cleanup_count; });
+            test::expect_equal(cleanup_count, 0);
+        }
+        test::expect_equal(cleanup_count, 1);
+
+        {
+            api::scope_guard_t guard([&] { ++cleanup_count; });
+            guard.release();
+            guard.release();
+        }
+        test::expect_equal(cleanup_count, 1);
+
+        test::expect_no_throw([] {
+            api::scope_guard_t guard(std::function<void()>());
+        });
     });
 }
